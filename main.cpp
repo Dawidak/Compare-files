37d11aed76665eaf546068b9e69892cb576aa54d
Dawidak <57863506+Dawidak@users.noreply.github.com>
2022-06-23 16:00:09 +0200

    Progress update
    Now you can see which files are being compared and percentage of whole scan

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 #include <filesystem>
+#include <windows.h>
 
 using namespace std;
 
@@ -56,37 +57,86 @@ int comparefiles(string file1, string file2)
   return 3;
 }
 
-std::vector<string> files(128000);
+bool fileExists(string fileName) {//if can read file, then file exists
+  ifstream test(fileName);
+  return (test) ? true : false;
+}
 
-int listfiles() {
+vector<string> shrinkVector(vector<string> vec) { //takes whole string vector and deletes empty elements
+    while(vec.back().empty()){
+        vec.pop_back();
+    }
+
+    return vec;
+}
+
+vector<string> listFiles(vector<string> files) { //finds all files and directories in program folder, then returns them in vector<string>
     string path = "./";
     int i = 0;
+
+    cout << "Analyzing files..." << endl;
 
     for (const auto & file : std::filesystem::recursive_directory_iterator(path)) {
         //cout << file.path() << endl;
         files[i] = file.path().string();
         i++;
     }
-    cout << "done" << endl;
-    return EXIT_SUCCESS;
+    //cout << "done" << endl;
+    files = shrinkVector(files);
+//goal 76 files ||||
+    for(unsigned int i = 0; i < files.size(); i++){
+        if(!fileExists(files[i])){
+            //cout << "deleting " + files[i] << endl;
+            files[i].swap(files.back());
+            files.pop_back();
+        }
+    }
+
+    cout << "Number of files: " << files.size() << endl;
+
+    return files;
 }
 
-bool fileExists(const string fileName)
-{
-  std::ifstream test(fileName);
-  return (test) ? true : false;
+int getWindowColumns(){
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    unsigned int columns;
+
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+    columns = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+
+    return columns;
 }
 
-int main() {
-    listfiles();
+int main(int argc, char *argv[]) {
+    int maxarraysize = 128000;
+    vector<string> files(maxarraysize);
 
-    for(int i = 0; i < 128000; i++){
+    files = listFiles(files);
+    vector<string> samefiles(1);
+    int precentage = 0;
+
+    for(unsigned int i = 0; i < files.size(); i++){
         if (fileExists(files[i]) /* && files[i].find("jpg") != std::string::npos */ ){ //commented code can be used to compare only some files
             //cout << "this is a file" << endl;
-            for(int j = 0; j < 128000; j++){
+            for(unsigned int j = 0; j < files.size(); j++){
                 if (i != j && fileExists(files[j]) /* && files[i].find("jpg") != std::string::npos */ ){ //commented code can be used to compare only some files
+
+                    unsigned int columns = getWindowColumns();
+
+                    cout << "\r";
+                    string spaces = "";
+                    for(unsigned int c = 0; c < columns; c++){
+                       spaces += " ";
+                    }
+                    cout << spaces;
+
+                    precentage = ((i * 1.0 + 1) / files.size())*100;
+                    string text = to_string(precentage) + "% | Comparing " + files[i] + " to " + files[j];
+                    text.resize(columns);
+                    cout << "\r" + text;
                     if(comparefiles(files[i], files[j]) == 3){
-                        cout << files[i] + " is the same as " + files[j] << endl;
+                        //cout << files[i] + " is the same as " + files[j] << endl;
+                        samefiles.push_back(files[i] + " is the same as " + files[j]);
                     }
                 }
             }
@@ -94,6 +144,10 @@ int main() {
         else{
             //cout << "directory found" << endl;
         }
+    }
+    cout << endl;
+    for(unsigned int i = 0; i < samefiles.size(); i++){
+        cout << samefiles[i] << endl;
     }
 
     return 0;
